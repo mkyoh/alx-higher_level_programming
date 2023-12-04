c04cc98f5c045e5e8b6e82ee48da958578cc6dbc
Mesmak <ymesmak@gmail.com>
2023-12-04 04:35:26 -0800

    add calculator_1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if__name__=="__main__":
     """print the addion, difference,product and quotient of 10 and 5."""
-    from calculator_1 import sum,sub,mul,div
+    from calculator_1 import add,sub,mul,div
+
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a,b,sum(a,b)))
+    print("{} + {} = {}".format(a,b,add(a,b)))
     print("{} - {} = {}".format(a,b,sub(a,b)))
     print("{} * {} = {}".format(a,b,mul(a,b)))
     print("{} / {} = {}".format(a,b,div(a,b)))
