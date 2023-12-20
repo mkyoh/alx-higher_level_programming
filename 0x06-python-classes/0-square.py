337d5ba7c622452019edb1f52420826c0f88dd52
Mesmak <ymesmak@gmail.com>
2023-12-19 16:25:43 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-'''Define a class square."""
+'''Define a Class Square."""
 
-class square:
-    """Represent a square."""
-    pass
+class Square:
+"""Represent a Square."""
+pass
